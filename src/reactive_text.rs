f575f73daffb4dfcf937c0763b16a07bf7bbbdcf
arlohb <arlo@urwick.co.uk>
2022-07-30 14:30:22 +0100

    Improved bar a bit

--- src/reactive_text.rs
+++ src/reactive_text.rs
@@ -1,4 +1,7 @@
-use penrose::draw::{DrawContext, DrawError, TextStyle, Widget};
+use penrose::{
+    __test_helpers::Region,
+    draw::{DrawContext, DrawError, TextStyle, Widget},
+};
 
 pub enum Align {
     Left,
@@ -11,6 +14,7 @@ pub struct ReactiveText {
     text_style: TextStyle,
     align: Align,
     extent: Option<(f64, f64)>,
+    screen_dimensions: Option<Vec<Region>>,
     last_updated: std::time::Instant,
     update_interval: std::time::Duration,
 }
@@ -27,6 +31,7 @@ impl ReactiveText {
             text_style,
             align,
             extent: None,
+            screen_dimensions: None,
             last_updated: std::time::Instant::now(),
             update_interval,
         })
@@ -35,30 +40,61 @@ impl ReactiveText {
     pub fn text(&mut self) -> String {
         (self.text)().unwrap_or_else(|| "".to_string())
     }
+
+    fn calc_extent(&mut self, ctx: &mut dyn DrawContext, _h: f64) -> Result<(f64, f64), DrawError> {
+        let (l, r) = self.text_style.padding;
+
+        ctx.font(&self.text_style.font, self.text_style.point_size)?;
+
+        let (w, h) = ctx.text_extent(&self.text())?;
+        let extent = (w + l + r + 0.1, h + 0.1);
+
+        Ok(extent)
+    }
 }
 
-impl<X> penrose::core::Hook<X> for ReactiveText where X: penrose::core::xconnection::XConn {}
+impl<X> penrose::core::Hook<X> for ReactiveText
+where
+    X: penrose::core::xconnection::XConn,
+{
+    fn screens_updated(
+        &mut self,
+        _wm: &mut penrose::WindowManager<X>,
+        dimensions: &[Region],
+    ) -> penrose::Result<()> {
+        self.screen_dimensions = Some(dimensions.to_vec());
+        Ok(())
+    }
+}
 
 impl Widget for ReactiveText {
     fn draw(
         &mut self,
         ctx: &mut dyn DrawContext,
-        _screen: usize,
+        screen: usize,
         _screen_has_focus: bool,
-        w: f64,
+        _w: f64,
         h: f64,
     ) -> Result<(), DrawError> {
+        let screen_dimensions = self.screen_dimensions.as_ref().ok_or_else(|| {
+            DrawError::Raw("Screen dimensions haven't been calculated yet".to_owned())
+        })?;
+        let screen_size = screen_dimensions[screen];
+
+        // Update the text.
+        let text = self.text();
+
+        // Recalculate the extent with the new text.
+        self.extent = Some(self.calc_extent(ctx, h)?);
+        let extent = self.extent.unwrap();
+
         ctx.font(&self.text_style.font, self.text_style.point_size)?;
         ctx.color(&self.text_style.fg);
 
-        let text = self.text();
-
-        let extent = self.current_extent(ctx, h)?;
-
         ctx.set_x_offset(match self.align {
             Align::Left => 0.,
-            Align::Center => (w - extent.0) / 2.,
-            Align::Right => w - extent.0,
+            Align::Center => (screen_size.w as f64 - extent.0) / 2.,
+            Align::Right => screen_size.w as f64 - extent.0,
         });
 
         ctx.text(&text, 1., self.text_style.padding)?;
@@ -76,10 +112,7 @@ impl Widget for ReactiveText {
         match self.extent {
             Some(extent) => Ok(extent),
             None => {
-                let (l, r) = self.text_style.padding;
-                ctx.font(&self.text_style.font, self.text_style.point_size)?;
-                let (w, h) = ctx.text_extent(&self.text())?;
-                let extent = (w + l + r + 0.1, h + 0.1);
+                let extent = self.calc_extent(ctx, _h)?;
                 self.extent = Some(extent);
                 Ok(extent)
             }
