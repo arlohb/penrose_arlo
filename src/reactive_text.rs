dffeb1cb70c9993b1ea7c54d28c367786f3c5f34
arlohb <arlo@urwick.co.uk>
2022-07-31 21:58:10 +0100

    Cleaned up ReactiveText

--- src/reactive_text.rs
+++ src/reactive_text.rs
@@ -12,8 +12,6 @@ pub struct ReactiveText {
     text: Box<dyn FnMut() -> Option<String>>,
     text_style: TextStyle,
     align: Align,
-    extent: Option<(f64, f64)>,
-    last_updated: std::time::Instant,
 }
 
 impl ReactiveText {
@@ -26,27 +24,11 @@ impl ReactiveText {
             text: Box::new(text),
             text_style,
             align,
-            extent: None,
-            last_updated: std::time::Instant::now(),
         })
     }
 
     pub fn text(&mut self) -> String {
         (self.text)().unwrap_or_else(|| "".to_string())
-    }
-
-    fn calc_extent(
-        &mut self,
-        ctx: &mut dyn DrawContext,
-    ) -> Result<(f64, f64), penrose::draw::Error> {
-        let (l, r) = self.text_style.padding;
-
-        ctx.font(&self.text_style.font, self.text_style.point_size)?;
-
-        let (w, h) = ctx.text_extent(&self.text())?;
-        let extent = (w + l + r + 0.1, h + 0.1);
-
-        Ok(extent)
     }
 }
 
@@ -56,13 +38,8 @@ impl BarWidget for ReactiveText {
         ctx: &mut dyn DrawContext,
         bar_width: f64,
         _bar_height: f64,
-    ) -> Result<(), penrose::draw::Error> {
-        // Update the text.
-        let text = self.text();
-
-        // Recalculate the extent with the new text.
-        self.extent = Some(self.calc_extent(ctx)?);
-        let extent = self.extent.unwrap();
+    ) -> penrose::draw::Result<()> {
+        let extent = self.current_extent(ctx)?;
 
         ctx.font(&self.text_style.font, self.text_style.point_size)?;
         ctx.color(&self.text_style.fg);
@@ -73,24 +50,19 @@ impl BarWidget for ReactiveText {
             Align::Right => bar_width - extent.0,
         });
 
-        ctx.text(&text, 1., self.text_style.padding)?;
-
-        self.last_updated = std::time::Instant::now();
+        ctx.text(&self.text(), 1., self.text_style.padding)?;
 
         Ok(())
     }
 
-    fn current_extent(
-        &mut self,
-        ctx: &mut dyn DrawContext,
-    ) -> Result<(f64, f64), penrose::draw::Error> {
-        match self.extent {
-            Some(extent) => Ok(extent),
-            None => {
-                let extent = self.calc_extent(ctx)?;
-                self.extent = Some(extent);
-                Ok(extent)
-            }
-        }
+    fn current_extent(&mut self, ctx: &mut dyn DrawContext) -> penrose::draw::Result<(f64, f64)> {
+        let (l, r) = self.text_style.padding;
+
+        ctx.font(&self.text_style.font, self.text_style.point_size)?;
+
+        let (w, h) = ctx.text_extent(&self.text())?;
+        let extent = (w + l + r + 0.1, h + 0.1);
+
+        Ok(extent)
     }
 }
